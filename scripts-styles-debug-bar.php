bbe53224c6613dc9d0b5820536c968f2adb963c6
Sudeep Kumar <sudeep333@gmail.com>
2015-01-29 21:49:22 +0530

    Initial Commit

---
+++ scripts-styles-debug-bar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Scripts Styles Debug Bar
+ * Plugin URI: http://sudeeprana.com
+ * Description: A tiny plugin to display all enqueued scripts and styles. debug-bar Dependent. Little help for Plugin/Theme Developers.
+ * Version: 1.0
+ * Author: Sudeep Kumar Rana
+ * Author URI: http://sudeeprana.com
+ */
+include "functions.php";
+?>
